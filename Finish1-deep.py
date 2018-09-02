f384b4c2f8d830455de8f15e335f14ad2eb4ecdf
ebrahimi_aida <ebrahimi_emerald@yahoo.com>
2018-09-02 23:23:35 +0430

    'Final'

--- Finish1-deep.py
+++ Finish1-deep.py
@@ -5,13 +5,13 @@ Created on Sat Aug 18 09:25:40 2018
 @author: Aida Ebrahimi
 """
 
-#-----------------
+#------miRNA-DeepLearning--Project-Str-------
 import keras
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 
-#---def-plot
+#---def-plot-------------
 def plot_Accuracy(net_history):
     history=net_history.history
     accuracy=history['acc']
@@ -35,12 +35,12 @@ def plot_Loss(net_history1):
          plt.legend(['losses','val_losses'])
          plt.show()            
    
-#-------- import data -------
+#-------- import data ------
 dataset=pd.read_csv("Final.csv")
 X=dataset.iloc[:,:-1].values
 y=dataset.iloc[:,1882].values
 
-#----categorical-XLabel-------------
+#----categorical-XLabel------
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
@@ -48,7 +48,7 @@ onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 X=X[:,1:]
 
-#----categorical-yLabel-------------
+#----categorical-yLabel------
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 from keras.utils import np_utils
@@ -65,31 +65,31 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)
 
-#----------
+#---Start-DeepLearning------
 from keras.models import Sequential
 from keras.layers import Dense,Dropout
-
-
 myModel=Sequential()
-myModel.add(Dense(1600,activation='relu',input_shape=(1882,)))
+myModel.add(Dense(1500,activation='relu',input_shape=(1882,)))
 myModel.add(Dropout(0.2))
-myModel.add(Dense(1250,activation='relu'))
+myModel.add(Dense(600,activation='relu'))
 myModel.add(Dropout(0.2))
-myModel.add(Dense(650,activation='relu'))
+myModel.add(Dense(300,activation='relu'))
 myModel.add(Dropout(0.2))
-myModel.add(Dense(327,activation='relu'))
+myModel.add(Dense(100,activation='relu'))
 myModel.add(Dropout(0.2))
 myModel.add(Dense(4,activation='softmax'))
 
-
+#--Model--Summary
 myModel.summary()
 
+#----------Compile model-----
+myModel.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'] )
 
-myModel.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
-
-#----------------------
-
-network_history=myModel.fit(X_train,y_train,epochs=1000,batch_size=125,shuffle=True,validation_data=(X_test,y_test))
+#----------fit-model---------
+network_history=myModel.fit(X_train,y_train,epochs=10,batch_size=125,verbose=1,shuffle=True,validation_data=(X_test,y_test))
+#---finish-deep learning------ 
+ 
+#----Examine Model--import new DATA------
 new=pd.read_excel('example.xlsx')
 new=new.iloc[:,:].values
 labelencoder_X = LabelEncoder()
@@ -99,12 +99,35 @@ new = onehotencoder.fit_transform(new).toarray()
 new=sc_X.transform(new)
 new_pred=myModel.predict(new)
 
+#---Examine--Model--TestSet---------------
 y_pred=np.argmax(myModel.predict(X_test),axis=1)
 y_pred=labelencoder_y.inverse_transform(y_pred)
 y_true=np.argmax(y_test,axis=1)
 y_true=labelencoder_y.inverse_transform(y_true)
+
+#----Confusion--MAtrix--------------------
 from sklearn.metrics import confusion_matrix
-cm=confusion_matrix(y_true,y_pred)
-#-------SHOW
+cmNN=confusion_matrix(y_true,y_pred)
+
+#-------SHOW--------------
 plot_Loss(network_history)
 plot_Accuracy(network_history)
+#---------------
+
+#------miRNA-DeepLearning--Project-Fin------
+from keras.models import load_model
+myModel= load_model('my_model1.h5')
+myModel.save('my_model1.h5')
+#--Classifier---------------------------------
+from sklearn.svm import SVC
+classifier=SVC(kernel='rbf',random_state=0 )
+classifier.fit(X_train,np.argmax(y_train,axis=1))
+#--KFold
+from sklearn.model_selection import cross_val_score
+accuracies=cross_val_score(estimator=classifier,X=X_train,y=np.argmax(y_train,axis=1),cv=10)
+accuracies.mean()
+#____________
+#----Confusion--MAtrix---------------------
+from sklearn.metrics import confusion_matrix
+cmClassifier=confusion_matrix(labelencoder_y.inverse_transform(classifier.predict(X_test)),y_true)
+#--------------
